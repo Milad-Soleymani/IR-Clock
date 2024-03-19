b133d3255725775ca22a357fe39494289481fd80
MiladCoder <miladcoder1390@gmail.com>
2024-03-19 21:19:00 +0330

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function updateClock (){
-    const now = new Date();
-    const hour = now.getHours().toString().padStart(2, 0);
-    const minute = now.getMinutes().toString().padStart(2, 0);
-    const secenod = now.getSeconds().toString().padStart(2, 0);
-    const timeString = `${hour}:${minute}:${secenod}`
-    document.getElementById("clock").textContent = timeString;
-  }
+const body = document.getElementById("body")
+
+body.style.backgroundImage = 'url(bg0.jpg)';
+
+// setInterval( ,3000)
+setInterval(() => {
+  let indexImg = Math.random() * 5
+  indexImg.toFixed(2)
   
-  updateClock(); 
-  setInterval(updateClock, 1000)
+  console.log(indexImg.toFixed(0))
+
+  body.style.backgroundImage = `url(bg${indexImg.toFixed(0)}.jpg)`
+}
+  , 2000)
+
+function updateClock() {
+  const now = new Date();
+  const hour = now.getHours().toString().padStart(2, 0);
+  const minute = now.getMinutes().toString().padStart(2, 0);
+  const secenod = now.getSeconds().toString().padStart(2, 0);
+  const timeString = `${hour}:${minute}:${secenod}`
+  document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000)
+// setInterval(bgImg, 1000)
